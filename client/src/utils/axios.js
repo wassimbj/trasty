377371ca693bdfcdf24efd733a1147373ffb8303
wassim <wassimbenjdida@gmail.com>
2022-01-06 16:10:25 +0100

    axios baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import constants from "../constants";
 
 const axiosInstance = axios.create({
-  // baseURL: constants.SERVER_URL,
-  baseURL: "https://trasty.herokuapp.com",
+  baseURL: constants.SERVER_URL,
+  // baseURL: "https://trasty.herokuapp.com",
   withCredentials: true,
   // headers: {
   //    'X-CSRF-TOKEN': Cookies.get('csrfToken'),
